f781cd34ae8b399280ea370b4697970f64db792e
wuc <XyURecHQGs2r3oad>
2023-11-01 10:09:04 +0800

    添加注释

--- client/src/main/java/org/apache/rocketmq/client/consumer/store/OffsetStore.java
+++ client/src/main/java/org/apache/rocketmq/client/consumer/store/OffsetStore.java
@@ -24,20 +24,27 @@ import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
 /**
+ * 消息消费进度存储
  * Offset store interface
  */
 public interface OffsetStore {
     /**
-     * Load
+     * 从本地加载消费偏移量Load
      */
     void load() throws MQClientException;
 
     /**
+     * 在内存种更新消费进度
      * Update the offset,store it in memory
      */
     void updateOffset(final MessageQueue mq, final long offset, final boolean increaseOnly);
 
     /**
+     * 获取消费进度 可选获取来源
+     *  READ_FROM_MEMORY 从内存中获取 ，
+     *  READ_FROM_STORE 从存储中心获取（本地文件 或者远程仓库 ），
+     *  MEMORY_FIRST_THEN_STORE 先内存，内存没有就数据中心
+     *
      * Get offset from local storage
      *
      * @return The fetched offset
@@ -45,26 +52,31 @@ public interface OffsetStore {
     long readOffset(final MessageQueue mq, final ReadOffsetType type);
 
     /**
+     * 存储所有消费进度 （集群模式就上传到broker, 广播模式就保存到本地缓存文件）
      * Persist all offsets,may be in local storage or remote name server
      */
     void persistAll(final Set<MessageQueue> mqs);
 
     /**
+     * 存储消费进度 （集群模式就上传到broker, 广播模式就保存到本地缓存文件）
      * Persist the offset,may be in local storage or remote name server
      */
     void persist(final MessageQueue mq);
 
     /**
+     * 删除消费进度
      * Remove offset
      */
     void removeOffset(MessageQueue mq);
 
     /**
+     * 克隆消费进度
      * @return The cloned offset table of given topic
      */
     Map<MessageQueue, Long> cloneOffsetTable(String topic);
 
     /**
+     *  向broker上传消费进度
      * @param mq
      * @param offset
      * @param isOneway
